1ae29c7fa587b89b8acc4ae9a9c225ac15b71f96
dobble11 <1103650146@qq.com>
2018-06-13 23:22:05 +0800

    1.0.0

--- EShop/client/src/components/demo.vue
+++
@@ -1,8 +0,0 @@
-﻿<template>
-    <div>
-        <form action="/api/upload" method="post" enctype="multipart/form-data">
-            <input type="file" name="files" multiple/>
-            <input type="submit" value="提交"/>
-        </form>
-    </div>
-</template>
